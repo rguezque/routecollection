e69169d5cb6ac6a6c4490614f355ed2bcdd886b2
Luis Rodríguez <rguezque@gmail.com>
2024-10-25 23:46:15 -0600

    refactor: :recycle: Refactor PhpInputStream.php

--- src/PhpInputStream.php
+++ src/PhpInputStream.php
@@ -8,14 +8,34 @@
 
 namespace rguezque\RouteCollection;
 
+use InvalidArgumentException;
+
 /**
  * Represent a read-only stream
  * 
- * @method Collection getParsedStr() Parses the raw query string into variables
- * @method Collection getDecodedJson() Decode variables from a json string
- * @method string getRawData() Return the raw stream
+ * @method Collection|string getStream()
  */
 class PhpInputStream {
+    /**
+     * Flag to return php-input-stream raw data
+     * 
+     * @var int
+     */
+    const RAW_DATA = 1;
+
+    /**
+     * Flag to parse string php-input-stream
+     * 
+     * @var int
+     */
+    const PARSED_STRING = 2;
+
+    /**
+     * Flag to decode php-input-string from JSON format
+     * 
+     * @var int
+     */
+    const DECODED_JSON = 3;
 
     /**
      * Is a read-only stream that allows reading data from the requested body
@@ -29,11 +49,39 @@ class PhpInputStream {
     }
 
     /**
+     * Return the php input stream. Use flags PhpInputStream::RAW_DATA (default), PhpInputStream::PARSED_STRING or PhpInputStream::DECODED_JSON
+     * 
+     * With PhpInputStream::RAW_DATA returns the raw data, PhpInputStream::PARSED_STRING to parse the query string, PhpInputStream::DECODED_JSON to decode the stream when is a JSON string.
+     * The data is wrapped an retrieved in a Collection object, except for PhpInputStream::RAW_DATA
+     * 
+     * @param int $format Flag for data format type to retrieve.
+     * @return Collection|string
+     * @throws  InvalidArgumentException When the flag is not valid
+     */
+    public function getStream(int $format = PhpInputStream::RAW_DATA): Collection|string {
+        switch ($format) {
+            case self::RAW_DATA:
+                $data = $this->stream;
+                break;
+            case self::PARSED_STRING:
+                $data = $this->getParsedStr();
+                break;
+            case self::DECODED_JSON:
+                $data = $this->getDecodedJson();
+                break;
+            default:
+                throw new InvalidArgumentException('Invalid flag code for PHP Input Stream');
+        }
+
+        return $data;
+    }
+
+    /**
      * Return the parsed raw query string into variables
      * 
      * @return Collection
      */
-    public function getParsedStr(): Collection {
+    private function getParsedStr(): Collection {
         parse_str($this->stream, $data);
 
         return new Collection($data);
@@ -44,7 +92,7 @@ class PhpInputStream {
      * 
      * @return Collection
      */
-    public function getDecodedJson(): Collection {
+    private function getDecodedJson(): Collection {
         $data = json_decode($this->stream, true);
 
         return new Collection($data);
@@ -55,7 +103,7 @@ class PhpInputStream {
      * 
      * @return string
      */
-    public function getRawData(): string {
+    private function getRawData(): string {
         return $this->stream;
     }
 
